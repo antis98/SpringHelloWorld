0abf94d587448e52cfad9db94e1e1bf263e949b7
antis98 <irxvad@gmail.com>
2024-03-24 09:05:43 +0700

    Выполнение задачи

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,11 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld beanClone =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean == beanClone);
+        Cat cat = (Cat) applicationContext.getBean("cat");
+        Cat catClone = (Cat) applicationContext.getBean("cat");
+        System.out.println(cat == catClone);
     }
 }
